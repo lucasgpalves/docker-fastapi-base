0dc14f49136de0fdd02180107930000ef7754c54
Lucas Alves <alvez.lucas2702@gmail.com>
2025-02-08 12:30:05 -0300

    fix structure and add .ignore files

--- app/app.py
+++ app/app.py
@@ -1,8 +1,23 @@
+import os
+import uvicorn
 from fastapi import FastAPI
+from dotenv import load_dotenv
 
+load_dotenv()
+
+PORT = int(os.getenv("PORT", 8000))
 
 app = FastAPI()
 
 @app.get("/")
 async def ping():
     return {"message": "Pong"}
+
+def main():
+    print(f'\033[93m Starting server in http://localhost:{PORT} ... \033[00m')
+
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=PORT,
+    )
--- run.py
+++ run.py
@@ -1,17 +1,4 @@
-import uvicorn
-import os
-from app.app import app
-from dotenv import load_dotenv
-
-load_dotenv()
-
-PORT = int(os.getenv("PORT", 8000))
+from app.app import main
 
 if __name__ == "__main__":
-    print(f'\033[93m Starting server in http://localhost:{PORT} ... \033[00m')
-
-    uvicorn.run(
-        app,
-        host="0.0.0.0",
-        port=PORT,
-    )
+    main()
